2effcf55f9694454d54808067286c694a8f0ac0a
Thiago Lages de Alencar <thiagola92@gmail.com>
2017-08-02 18:05:51 -0300

    Criando a LoginActivity

--- Aplicativo/app/src/main/java/lylo/lylo/LoginActivity.java
+++ Aplicativo/app/src/main/java/lylo/lylo/LoginActivity.java
@@ -2,12 +2,30 @@ package lylo.lylo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+
+    public void tryLogin(View view) {
+        try {
+            login(view);
+        } catch(Exception e) {
+
+        }
+    }
+
+    public void login(View view) {
+        Log.v("LoginActivity", "Logando");
     }
 }
