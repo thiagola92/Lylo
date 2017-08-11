bba7c8dd1df869070d9a16424ac6df6e954afa0e
Thiago Lages de Alencar <thiagola92@gmail.com>
2017-08-11 20:03:50 -0300

    LoginActivity

--- Aplicativo/app/src/main/java/lylo/lylo/LoginActivity.java
+++ Aplicativo/app/src/main/java/lylo/lylo/LoginActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,13 +19,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Typeface font_montserrat_extra_light = Typeface.createFromAsset(getAssets(), "fonts/Montserrat_ExtraLight.ttf");
+        setFonts();
+    }
+
+    private void setFonts() {
+        Typeface font_montserrat_extra_light = Typeface.createFromAsset(getAssets(), "fonts/Montserrat-ExtraLight.ttf");
+        Typeface font_montserrat_medium = Typeface.createFromAsset(getAssets(), "fonts/Montserrat-Medium.ttf");
 
         TextView subtitle = (TextView) findViewById(R.id.activity_login_subtitle);
+        Button facebook = (Button) findViewById(R.id.activity_login_facebook);
+        Button register = (Button) findViewById(R.id.activity_login_register);
         TextView already_have_an_account = (TextView) findViewById(R.id.activity_login_already_have_an_account);
 
         subtitle.setTypeface(font_montserrat_extra_light);
-        already_have_an_account.setTypeface(font_montserrat_extra_light);
+        facebook.setTypeface(font_montserrat_medium);
+        register.setTypeface(font_montserrat_medium);
+        already_have_an_account.setTypeface(font_montserrat_medium);
     }
 
     protected void onResume() {
